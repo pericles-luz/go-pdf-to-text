100a1f424478f5d5634b22e22fb9909a5b731b16
Pericles Luz <pericles.luz@gmail.com>
2023-12-24 12:57:11 -0300

    feat: busca totais gerais

--- internal/parse_test/calculo_test.go
+++ internal/parse_test/calculo_test.go
@@ -30,3 +30,33 @@ func TestCalculoMustFindBaseData(t *testing.T) {
 	require.Equal(t, "03410702709", calculo.Cpf())
 	require.Equal(t, "11804009", calculo.IdUnica())
 }
+
+func TestCalculoMustFindDesagio35(t *testing.T) {
+	lines := readOriginFile(t)
+	calculo := application.NewCalculo()
+	err := parse.Desagio35(lines, calculo)
+	require.NoError(t, err)
+	require.Equal(t, uint64(134042), calculo.Desagio35().ValorCorrigido())
+	require.Equal(t, uint64(174848), calculo.Desagio35().ValorJurosMora())
+	require.Equal(t, uint64(308889), calculo.Desagio35().TotalDevido())
+}
+
+func TestCalculoMustFindTotalAposDesagio35(t *testing.T) {
+	lines := readOriginFile(t)
+	calculo := application.NewCalculo()
+	err := parse.TotalAposDesagio35(lines, calculo)
+	require.NoError(t, err)
+	require.Equal(t, uint64(248934), calculo.TotalAposDesagio35().ValorCorrigido())
+	require.Equal(t, uint64(324717), calculo.TotalAposDesagio35().ValorJurosMora())
+	require.Equal(t, uint64(573651), calculo.TotalAposDesagio35().TotalDevido())
+}
+
+func TestCalculoMustFindTotal(t *testing.T) {
+	lines := readOriginFile(t)
+	calculo := application.NewCalculo()
+	err := parse.Total(lines, calculo)
+	require.NoError(t, err)
+	require.Equal(t, uint64(382976), calculo.Total().ValorCorrigido())
+	require.Equal(t, uint64(499564), calculo.Total().ValorJurosMora())
+	require.Equal(t, uint64(882540), calculo.Total().TotalDevido())
+}
